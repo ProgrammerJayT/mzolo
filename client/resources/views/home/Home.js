eae9cd20fdcfca1b7bca3e46d01ea92483eca8a1
Programmer JayT <theanthem8@gmail.com>
2022-12-06 21:08:05 +0200

    Created home screen with helper index

---
+++ client/resources/views/home/Home.js
@@ -0,0 +1,16 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
+import {StyleSheet, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Text>Home screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
