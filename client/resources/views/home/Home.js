519306ec2987f3217a20a802a237c348b101a8b5
Programmer JayT <theanthem8@gmail.com>
2022-12-06 23:12:30 +0200

    Created home page

--- client/resources/views/home/Home.js
+++ client/resources/views/home/Home.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={styles.container}>
       <Text>Home screen</Text>
     </SafeAreaView>
   );
@@ -13,4 +13,11 @@ const Home = () => {
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
