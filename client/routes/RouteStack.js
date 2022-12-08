5b132581e2e7957171af0030b3fd299f76d4a6db
Programmer JayT <theanthem8@gmail.com>
2022-12-08 13:07:24 +0200

    Imported drawer stack component

--- client/routes/RouteStack.js
+++ client/routes/RouteStack.js
@@ -1,15 +1,15 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {View, Text} from 'react-native';
+import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from '../resources/views/home/Home';
 import Splash from '../resources/views/splash/Splash';
+import Home from '../resources/views/home/Home';
+import DrawerStack from './DrawerStack';
 
 const Stack = createNativeStackNavigator();
 
-function RouteStack() {
+const RouteStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -17,10 +17,10 @@ function RouteStack() {
           headerShown: false,
         }}>
         <Stack.Screen name="Splash" component={Splash} />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Drawer" component={DrawerStack} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default RouteStack;
